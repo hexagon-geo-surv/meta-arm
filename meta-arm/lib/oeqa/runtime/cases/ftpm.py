hexagon-geo-surv/meta-arm

commit d4507866677c3d32d5b862de141b36c26f275ee8

oeqa runtime: add optee.py test
The test runs xtest test suite from optee-tests package.

Signed-off-by: Mikko Rapeli <mikko.rapeli@linaro.org>
Signed-off-by: Jon Mason <jon.mason@arm.com>

meta-arm/lib/oeqa/runtime/cases/optee.py
@@ -1,0 +1,24 @@
+#
+# SPDX-License-Identifier: MIT
+#
+
+import os
+
+from oeqa.runtime.case import OERuntimeTestCase
+from oeqa.runtime.decorator.package import OEHasPackage
+from oeqa.core.decorator.oetimeout import OETimeout
+
+class OpteeTestSuite(OERuntimeTestCase):
+    """
+    Run OP-TEE tests (xtest).
+    """
+    @OETimeout(800)
+    @OEHasPackage(['optee-test'])
+    def test_opteetest_xtest(self):
+        # clear storage before executing tests
+        cmd = "xtest --clear-storage || true"
+        status, output = self.target.run(cmd, timeout=60)
+        self.assertEqual(status, 0, msg='\n'.join([cmd, output]))
+        cmd = "xtest"
+        status, output = self.target.run(cmd, timeout=600)
+        self.assertEqual(status, 0, msg='\n'.join([cmd, output]))

commit 7f04ddb3628ba3d095cb23db5dae9fff90e26d97

arm/oeqa: increase optee and ftpm test timeouts
OPTEE and ftpm tests are failing in CI on slower systems due to timing
out, but actually finish when given enough time to complete.  Increase
the timeout value to be roughly 100 seconds longer than the time it is
currently taking to finish on the slower systems.

Fixes: d4507866677c ("oeqa runtime: add optee.py test")
Fixes: ba315f7242e0 ("oeqa runtime: add ftpm.py test")
Signed-off-by: Jon Mason <jon.mason@arm.com>

meta-arm/lib/oeqa/runtime/cases/ftpm.py
@@ -11,12 +11,12 @@ class FtpmTestSuite(OERuntimeTestCase):
     """
     Minimal test for optee-ftpm and ftpm kernel driver interfaces
     """
-    @OETimeout(200)
+    @OETimeout(360)
     def test_ftpm(self):
         # device files, need tee-supplicant fully initialized which takes some time
         # and tests seem to run before boot is complete
-        cmd = "ls -l /dev/tpm0 /dev/tpmrm0 || ( runlevel; sleep 10; ls -l /dev/tpm0 /dev/tpmrm0 )"
-        status, output = self.target.run(cmd, timeout=60)
+        cmd = "ls -l /dev/tpm0 /dev/tpmrm0 || ( runlevel; sleep 60; ls -l /dev/tpm0 /dev/tpmrm0 )"
+        status, output = self.target.run(cmd, timeout=90)
         self.assertEqual(status, 0, msg='\n'.join([cmd, output]))
 
         # tpm version

meta-arm/lib/oeqa/runtime/cases/optee.py
@@ -12,7 +12,7 @@ class OpteeTestSuite(OERuntimeTestCase):
     """
     Run OP-TEE tests (xtest).
     """
-    @OETimeout(800)
+    @OETimeout(1300)
     @OEHasPackage(['optee-test'])
     def test_opteetest_xtest(self):
         # clear storage before executing tests
@@ -20,5 +20,5 @@ class OpteeTestSuite(OERuntimeTestCase):
         status, output = self.target.run(cmd, timeout=60)
         self.assertEqual(status, 0, msg='\n'.join([cmd, output]))
         cmd = "xtest"
-        status, output = self.target.run(cmd, timeout=600)
+        status, output = self.target.run(cmd, timeout=1200)
         self.assertEqual(status, 0, msg='\n'.join([cmd, output]))